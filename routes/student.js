b9997d810f2f7087d2e9ba7364ff73a6067b8fbf
Paul Vu <vudpaul@gmail.com>
2018-10-16 21:38:17 -0400

    Add ability to edit students

--- routes/student.js
+++ routes/student.js
@@ -4,6 +4,7 @@ var express = require('express'),
     myFunctions = require('../public/main');
     router = express.Router();
 
+//Show all students
 router.get('/', middleware.isLoggedIn, function(req, res) {
     Student.find({}, function(err, allStudents) {
         if(err) {
@@ -14,10 +15,12 @@ router.get('/', middleware.isLoggedIn, function(req, res) {
     });
 });
 
+//Show new student form
 router.get('/new', middleware.isLoggedIn,function(req, res) {
     res.render('student/new');
 });
 
+//Create new student
 router.post('/', middleware.isLoggedIn, function(req, res) {
     Student.create({
         firstName: req.body.student_first_name,
@@ -34,6 +37,7 @@ router.post('/', middleware.isLoggedIn, function(req, res) {
     })
 });
 
+//Show more information about student
 router.get('/:id', middleware.isLoggedIn, function(req, res) {
     Student.findById(req.params.id).exec(function(err, foundStudent) {
         if(err || !foundStudent) {
@@ -41,6 +45,30 @@ router.get('/:id', middleware.isLoggedIn, function(req, res) {
             res.redirect('/students');
         } else {
             res.render('student/show', {student: foundStudent})
+        }
+    });
+});
+
+//Get student edit form
+router.get('/:id/edit', function(req, res) {
+    Student.findById(req.params.id, function(err, foundStudent) {
+        if(err) {
+            res.redirect('/');
+        } else {
+            res.render('student/edit', {student: foundStudent});
+        }
+    });
+});
+
+//UPDATE student
+router.put('/:id', function(req, res) {
+    var student = req.body.student;
+    Student.findByIdAndUpdate(req.params.id, student, function(err, updatedStudent) {
+        if(err) {
+            res.redirect('/students/' + req.params.id +'/edit');
+        } else {
+            req.flash('success', 'Successfully updated student profile!');
+            res.redirect('/students/' + req.params.id);
         }
     });
 });
