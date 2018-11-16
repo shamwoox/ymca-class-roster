e6043047ff59a147ec6f3fe01d9d50d83d56ab54
Paul Vu <vudpaul@gmail.com>
2018-11-16 16:56:50 -0500

    Futher patches

--- routes/session.js
+++ routes/session.js
@@ -6,127 +6,127 @@ var express = require('express'),
     Class = require('../models/class'),
     router = express.Router();
 
-    const accountSid = 'AC529af1fbe70b475bec790a3272ceda2d';
-    const authToken = '96f43643baa1c44addf58d3f3aad95e9';
-    const client = require('twilio')(accountSid, authToken);
-    const classes = [
-        {
-            name: "A / Water Discovery",
-            skills: [
-                "Blow bubbles on surface, assisted",
-                "Front tow chin in water, assisted",
-                "Water exit parent & child together",
-                "Water entry parent & child together",
-                "Back float assisted, head on shoulder",
-                "Roll assisted, back to front & front to back",
-                "Front float chin in water, assisted",
-                "Back tow assisted, head on shoulder",
-                "Wall grab assisted"
-            ]
-        },
-        {
-            name: "B / Water Exploration",
-            skills: [
-                "Blow bubbles mouth and nose submerged, assisted",
-                "Front tow blow bubbles, assisted",
-                "Water exit assisted",
-                "Water entry assisted",
-                "Back float assisted, head on chest",
-                "Roll assisted, back to front & front to back",
-                "Front float blow bubbles, assisted",
-                "Back tow assisted, head on chest",
-                "Monkey crawl assisted, on edge, 5ft."
-            ]
-        },
-        {
-            name: "1 / Acclimation",
-            skills: [
-                "Submerge bob independently",
-                "Front glide assisted, to wall, 5ft.",
-                "Water exit independently",
-                "Jump, push, turn, grab assisted",
-                "Back float assisted, 10 secs, recover independently",
-                "Roll assisted, back to front & front to back",
-                "Front float assisted, 10 secs, recover independently",
-                "Back glide assisted, at wall, 5ft.",
-                "Swim, float, swim assisted, 10 ft."
-            ]
-        },
-        {
-            name: "2 / Water Movement",
-            skills: [
-                "Submerge look at object on bottom",
-                "Front glide 10 ft. (5 ft. preschool)",
-                "Water exit independently",
-                "Jump, push, turn, grab",
-                "Back float 20 secs. (10 secs. preschool)",
-                "Roll back to front & front to back",
-                "Front float 20 secs. (10 secs. preschool)",
-                "Back glide 10 ft. (5 ft. preschool)",
-                "Tread water 10 secs, near wall, & exit",
-                "Swim, float, swim 5 yd."
-            ]
-        },
-        {
-            name: "3 / Water Stamina",
-            skills: [
-                "Submerge retrieve objec in chest-deep water",
-                "Swim on front 15 yd. (10 yd. preschool)",
-                "Water exit independently",
-                "Jump, swim, turn, swim, grab 10 yd.",
-                "Swim on back  15 yd. (10 yd. preschool)",
-                "Roll back to front & front to back",
-                "Tread water 1 min. & exit (30 secs. preschool)",
-                "Swim, float, swim 25 yd. (15 yd. preschool)"
-            ]
-        },
-        {
-            name: "4 / Stroke Introduction",
-            skills: [
-                "Endurance any stroke or combination of strokes, 25 yd.",
-                "Front crawl rotary breathing, 15 yd.",
-                "Back crawl 15 yd.",
-                "Dive sitting",
-                "Resting stroke elementary backstroke, 15 yd.",
-                "Tread water scissor & whip kick, 1 min.",
-                "Breaststroke kick, 15 yd.",
-                "Butterfly kick, 15 yd."
-            ]
-        },
-        {
-            name: "5 / Stroke Development",
-            skills: [
-                "Endurance any stroke or combination of strokes, 50 yd.",
-                "Front crawl bent-arm recovery, 25 yd.",
-                "Back crawl pull, 25 yd.",
-                "Dive kneeling",
-                "Resting stroke sidestroke, 25 yd.",
-                "Tread water scissor & whip kick, 2 min.",
-                "Breaststroke 25 yd.",
-                "Butterfly simultaneous arm action & kick, 15 yd."
-            ]
-        },
-        {
-            name: "6 / Stroke Mechanics",
-            skills: [
-                "Endurance any stroke or combination of strokes, 150 yd.",
-                "Front crawl flip-turn, 50 yd.",
-                "Back crawl pull and flip-turn, 50 yd.",
-                "Dive standing",
-                "Resting stroke elementary backstroke or sidestroke, 50 yd.",
-                "Tread water retreive object off bottom, tread 1 min.",
-                "Breaststroke open turn, 50 yd.",
-                "Butterfly 25 yd."
-            ]
-        }
-    ]
+const accountSid = 'AC529af1fbe70b475bec790a3272ceda2d';
+const authToken = '96f43643baa1c44addf58d3f3aad95e9';
+const client = require('twilio')(accountSid, authToken);
+const classes = [
+    {
+        name: "A / Water Discovery",
+        skills: [
+            "Blow bubbles on surface, assisted",
+            "Front tow chin in water, assisted",
+            "Water exit parent & child together",
+            "Water entry parent & child together",
+            "Back float assisted, head on shoulder",
+            "Roll assisted, back to front & front to back",
+            "Front float chin in water, assisted",
+            "Back tow assisted, head on shoulder",
+            "Wall grab assisted"
+        ]
+    },
+    {
+        name: "B / Water Exploration",
+        skills: [
+            "Blow bubbles mouth and nose submerged, assisted",
+            "Front tow blow bubbles, assisted",
+            "Water exit assisted",
+            "Water entry assisted",
+            "Back float assisted, head on chest",
+            "Roll assisted, back to front & front to back",
+            "Front float blow bubbles, assisted",
+            "Back tow assisted, head on chest",
+            "Monkey crawl assisted, on edge, 5ft."
+        ]
+    },
+    {
+        name: "1 / Acclimation",
+        skills: [
+            "Submerge bob independently",
+            "Front glide assisted, to wall, 5ft.",
+            "Water exit independently",
+            "Jump, push, turn, grab assisted",
+            "Back float assisted, 10 secs, recover independently",
+            "Roll assisted, back to front & front to back",
+            "Front float assisted, 10 secs, recover independently",
+            "Back glide assisted, at wall, 5ft.",
+            "Swim, float, swim assisted, 10 ft."
+        ]
+    },
+    {
+        name: "2 / Water Movement",
+        skills: [
+            "Submerge look at object on bottom",
+            "Front glide 10 ft. (5 ft. preschool)",
+            "Water exit independently",
+            "Jump, push, turn, grab",
+            "Back float 20 secs. (10 secs. preschool)",
+            "Roll back to front & front to back",
+            "Front float 20 secs. (10 secs. preschool)",
+            "Back glide 10 ft. (5 ft. preschool)",
+            "Tread water 10 secs, near wall, & exit",
+            "Swim, float, swim 5 yd."
+        ]
+    },
+    {
+        name: "3 / Water Stamina",
+        skills: [
+            "Submerge retrieve objec in chest-deep water",
+            "Swim on front 15 yd. (10 yd. preschool)",
+            "Water exit independently",
+            "Jump, swim, turn, swim, grab 10 yd.",
+            "Swim on back  15 yd. (10 yd. preschool)",
+            "Roll back to front & front to back",
+            "Tread water 1 min. & exit (30 secs. preschool)",
+            "Swim, float, swim 25 yd. (15 yd. preschool)"
+        ]
+    },
+    {
+        name: "4 / Stroke Introduction",
+        skills: [
+            "Endurance any stroke or combination of strokes, 25 yd.",
+            "Front crawl rotary breathing, 15 yd.",
+            "Back crawl 15 yd.",
+            "Dive sitting",
+            "Resting stroke elementary backstroke, 15 yd.",
+            "Tread water scissor & whip kick, 1 min.",
+            "Breaststroke kick, 15 yd.",
+            "Butterfly kick, 15 yd."
+        ]
+    },
+    {
+        name: "5 / Stroke Development",
+        skills: [
+            "Endurance any stroke or combination of strokes, 50 yd.",
+            "Front crawl bent-arm recovery, 25 yd.",
+            "Back crawl pull, 25 yd.",
+            "Dive kneeling",
+            "Resting stroke sidestroke, 25 yd.",
+            "Tread water scissor & whip kick, 2 min.",
+            "Breaststroke 25 yd.",
+            "Butterfly simultaneous arm action & kick, 15 yd."
+        ]
+    },
+    {
+        name: "6 / Stroke Mechanics",
+        skills: [
+            "Endurance any stroke or combination of strokes, 150 yd.",
+            "Front crawl flip-turn, 50 yd.",
+            "Back crawl pull and flip-turn, 50 yd.",
+            "Dive standing",
+            "Resting stroke elementary backstroke or sidestroke, 50 yd.",
+            "Tread water retreive object off bottom, tread 1 min.",
+            "Breaststroke open turn, 50 yd.",
+            "Butterfly 25 yd."
+        ]
+    }
+]
 
-    const seasons = [
-        'Fall',
-        'Winter',
-        'Spring',
-        'Summer'
-    ];
+const seasons = [
+    'Fall',
+    'Winter',
+    'Spring',
+    'Summer'
+];
 
 router.get('/', middleware.isLoggedIn, function(req, res) {
     Session.find({}, function(err, allSessions) {
